8fbbb9e4f10828814b20e5d1f63344d3e39375ae
Donald Brooks <brooksd079@gmail.com>
2022-05-14 14:58:23 +0300

    update to css with minor changes to UI, added a hover to the button and dummy Js questions to the questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,38 +1,38 @@
 const TestQuestions = [
     {
-        question: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
+        question: 'Which type of JavaScript language is ___',
+        a: 'Object-Oriented',
+        b:'Object-based',
+        c:'Assembly-language',
+        d:'High-level',
+        correct: 'b'
+    }, {
+        question: 'Which one of the following also known as Conditional Expression:',
+        a: 'Alternative to if-else',
+        b:'Switch statement',
+        c:'if-then-else statement',
+        d:'immediate if',
+        correct: 'd'
+    }, {
+        question: 'In JavaScript, what is a block of statement?',
+        a: 'Conditional block',
+        b:'block combines a number of statements into a single compound statement',
+        c:'both conditional block and a single statement',
+        d:'block that contains a single statement',
         correct: ''
     }, {
-        question: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
-        correct: ''
+        question: ' When interpreter encounters an empty statements, what it will do:',
+        a: 'Shows a warning',
+        b:'Prompts to complete the statement',
+        c:'Throws the error',
+        d:'Ignores the statements ',
+        correct: 'd'
     }, {
-        question: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
-        correct: ''
-    }, {
-        question: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
-        correct: ''
-    }, {
-        question: '',
-        a: '',
-        b:'',
-        c:'',
-        d:'',
-        correct: ''
+        question: ' The "function" and " var" are known as:',
+        a: 'Keywords',
+        b:'Data types',
+        c:'Declaration statements',
+        d:'Prototypes',
+        correct: 'c'
     }
 ]
