ce8c9f4159dd1338b7be3c17f17e4128edcd5790
Donald Brooks <brooksd079@gmail.com>
2022-05-14 13:11:42 +0300

    update and implements the css on the questions box also removed some code for better understanding

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+const TestQuestions = [
+    {
+        question: '',
+        a: '',
+        b:'',
+        c:'',
+        d:'',
+        correct: ''
+    }, {
+        question: '',
+        a: '',
+        b:'',
+        c:'',
+        d:'',
+        correct: ''
+    }, {
+        question: '',
+        a: '',
+        b:'',
+        c:'',
+        d:'',
+        correct: ''
+    }, {
+        question: '',
+        a: '',
+        b:'',
+        c:'',
+        d:'',
+        correct: ''
+    }, {
+        question: '',
+        a: '',
+        b:'',
+        c:'',
+        d:'',
+        correct: ''
+    }
+]
